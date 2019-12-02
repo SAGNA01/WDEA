14e63527f8daab77fec17abce9f7c96d3c5771a3
SAGNA01 <sagnaboubou01@gmail.com>
2019-12-03 00:23:09 +0100

    definition du constructeur

--- SetUpParams.cpp
+++ SetUpParams.cpp
@@ -1,11 +1,9 @@
 #include "SetupParams.h"
 
-SetUpParams::SetUpParams()
-{
-    //ctor
-}
+SetUpParams::SetUpParams(unsigned int runs, unsigned int steps, unsigned int pop_size, unsigned int sol_size) :
+                         _independent_runs{runs}, _nb_evolution_steps{steps},
+                         _population_size{pop_size}, _solution_size{sol_size}
+{}
 
 SetUpParams::~SetUpParams()
-{
-    //dtor
-}
+{}
