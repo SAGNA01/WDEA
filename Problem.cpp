a0b54ae09e14d24dd86a24749db4515ec1d67608
asmaeya <asmae21yassine@gmail.com>
2019-12-09 15:00:03 +0100

    mod problem

--- Problem.cpp
+++ Problem.cpp
@@ -5,7 +5,14 @@ Problem::Problem()
     //ctor
 }
 
+Problem::Problem(int dimension, double LowerB, double UpperB): _dimension{dimension}, LowerBound {LowerB}, UpperBound {UpperB}{}
+
 Problem::~Problem()
 {
     //dtor
+}
+
+int Problem::Dimension() const
+{
+	return _dimension;
 }
